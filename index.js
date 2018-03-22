6973368c200a3d4a3b111e7e8d4671a55f9e87d6
Pierre <cpfreelance11@gmail.com>
2018-03-22 21:19:22 +0200

    added easter egg, finshed project

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
+    
+
+    $('#list-items').html(localStorage.getItem("listItems"));
+
+
     $(".add-items").submit(function(event) //event just so that it does not complain that event not there.
     {
 		event.preventDefault();
 
     	var item = $("#todo-list-item").val(); //grabs value and inserts here
 
-    	if (item)
+    	if(item == 'egg')
+        {
+
+            $('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + "<a href = 'http://gph.is/2ppfkc1'>Easter EGG!!</a>" + "<a class='remove'>X</a><hr></li>")
+            localStorage.setItem("listItems", $('#list-items').html())
+            $("#todo-list-item").val("");       //deletes value that is in here
+        }   
+        else
     	{
     		$('#list-items').append("<li><input class='checkbox' type='checkbox'/>" + item + "<a class='remove'>X</a><hr></li>")
- 			$("#todo-list-item").val("");   	//deletes value that is in here
-    	}
+ 			localStorage.setItem("listItems", $('#list-items').html())
+            $("#todo-list-item").val("");   	//deletes value that is in here
+
+        }
 
     	
 	});
@@ -20,13 +34,26 @@ $(document).ready(function () {
     // })
 
     $(document).on("change", ".checkbox", function() {
-
+        if($(this).attr("checked"))
+        {
+            $(this).removeAttr("checked");
+        }
+        else
+        {
+            $(this).attr("checked", "checked");
+        }
+     
     	$(this).parent().toggleClass("completed");
+         localStorage.setItem("listItems", $('#list-items').html());
     });
 
      $(document).on("click", ".remove", function() {
+          
+          $(this).fadeOut( "slow", function() {
+                    $(this).parent().remove();
+                    localStorage.setItem("listItems", $('#list-items').html());
+             });
 
-    	$(this).parent().remove();
     });
 
 });
